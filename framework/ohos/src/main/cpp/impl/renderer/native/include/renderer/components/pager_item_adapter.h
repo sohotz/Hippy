9b6aed37b14bd163833b742318b431f81277ec01
sohotz <sohotz@qq.com>
2024-10-24 12:34:50 +0800

    feat(ohos):  support lazy create node for pager and list

--- framework/ohos/src/main/cpp/impl/renderer/native/include/renderer/components/pager_item_adapter.h
+++ framework/ohos/src/main/cpp/impl/renderer/native/include/renderer/components/pager_item_adapter.h
@@ -103,6 +103,7 @@ private:
   void OnNewItemAttached(ArkUI_NodeAdapterEvent *event) {
     auto index = OH_ArkUI_NodeAdapterEvent_GetItemIndex(event);
     auto view = itemViews_[index];
+    view->CreateArkUINode(true, -1);
     ArkUI_NodeHandle handle = view->GetLocalRootArkUINode()->GetArkUINodeHandle();
     // 设置需要展示的元素。
     OH_ArkUI_NodeAdapterEvent_SetItem(event, handle);
